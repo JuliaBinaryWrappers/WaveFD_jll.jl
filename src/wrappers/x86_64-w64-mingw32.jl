f8048723194962ecf5704b94e3230f47812dbaf6
jlbuild <juliabuildbot@gmail.com>
2020-11-10 22:17:58 +0000

    WaveFD_jll build 0.2.0+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -2,6 +2,7 @@
 export libillumination, libprop2DAcoIsoDenQ_DEO2_FDTD, libprop2DAcoTTIDenQ_DEO2_FDTD, libprop2DAcoVTIDenQ_DEO2_FDTD, libprop3DAcoIsoDenQ_DEO2_FDTD, libprop3DAcoTTIDenQ_DEO2_FDTD, libprop3DAcoVTIDenQ_DEO2_FDTD, libspacetime
 
 using CompilerSupportLibraries_jll
+using FFTW_jll
 JLLWrappers.@generate_wrapper_header("WaveFD")
 JLLWrappers.@declare_library_product(libillumination, "libillumination.dll")
 JLLWrappers.@declare_library_product(libprop2DAcoIsoDenQ_DEO2_FDTD, "libprop2DAcoIsoDenQ_DEO2_FDTD.dll")
@@ -12,7 +13,7 @@ JLLWrappers.@declare_library_product(libprop3DAcoTTIDenQ_DEO2_FDTD, "libprop3DAc
 JLLWrappers.@declare_library_product(libprop3DAcoVTIDenQ_DEO2_FDTD, "libprop3DAcoVTIDenQ_DEO2_FDTD.dll")
 JLLWrappers.@declare_library_product(libspacetime, "libspacetime.dll")
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, FFTW_jll)
     JLLWrappers.@init_library_product(
         libillumination,
         "bin\\libillumination.dll",
